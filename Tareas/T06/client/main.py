dca6a171045bc8792e37a9399244a66f5f4d801c
rgriver <rgrivera@uc.cl>
2017-06-20 18:45:43 -0400

    Delete main.py

--- Tareas/T06/client/main.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtWidgets import QApplication
-from client.game import Game
-import sys
-
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    game = Game()
-    app.exec_()
