3bc3b5db86620c3f45edf3fd537697cc3ada492c
rgriver <rgrivera@uc.cl>
2017-06-20 18:38:47 -0400

    Delete points_calculator.py

--- Tareas/T06/server/points_calculator.py
+++
@@ -1,9 +0,0 @@
-class PointsCalculator:
-    def __init__(self, room):
-        self.users = room.users
-        self.scoreboard = room.scoreboard
-
-    def calculate(self, username, state, answer_time):
-        points = state * (20 - answer_time)
-        self.scoreboard[username] += points
-        return points
