6d2b7d4768f083933f692f1ab18af7aac48e6c87
Rodrigo Rivera <rgrivera@uc.cl>
2017-06-23 22:08:02 -0400

    update

--- Tareas/T07/main.py
+++ Tareas/T07/main.py
@@ -48,11 +48,15 @@ def handle_telegram_event():
     chat_id = data['chat']['id']
     text = data['message']['text']
     bot_controller.send_message(375779180, 'Damn son')
+    return 'ok'
 
 
-@app.route('/')
+@app.route('/', methods=['POST'])
 def index():
-    # bot_controller.send_message(375779180, 'sendMessage')
+    data = json.loads(flask.request.data)
+    chat_id = data['chat']['id']
+    text = data['message']['text']
+    bot_controller.send_message(375779180, 'Damn son')
     return 'ok'
 
 
