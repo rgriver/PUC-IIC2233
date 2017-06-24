39c08e55ef5ac5be6da15da85cc7375633b21fec
Rodrigo Rivera <rgrivera@uc.cl>
2017-06-24 03:25:58 -0400

    update

--- Tareas/T07/main.py
+++ Tareas/T07/main.py
@@ -72,7 +72,6 @@ requests.post(base_url, data=webhook_data)
 def handle_github_event():
     try:
         data = json.loads(flask.request.data)
-        bot_controller.send_message(375779180, str(data.keys()))
         action = data['action']
         if action is 'opened':
             bot_controller.notify_of_issue_opening(data['issue'])
