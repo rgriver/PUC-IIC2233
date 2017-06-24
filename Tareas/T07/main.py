b6097c399b8bb3df4d5e61f47baac4207c62c26f
Rodrigo Rivera <rgrivera@uc.cl>
2017-06-23 23:50:53 -0400

    update

--- Tareas/T07/main.py
+++ Tareas/T07/main.py
@@ -57,9 +57,10 @@ def handle_github_event():
 
 @app.route('/telegram', methods=['POST'])
 def handle_telegram_event():
-    data = json.loads(flask.request.data)
-    chat_id = data['chat']['id']
-    text = data['message']['text']
+    return flask.Response(status=200)
+    # data = json.loads(flask.request.data)
+    #chat_id = data['chat']['id']
+    # text = data['message']['text']
     bot_controller.send_message(375779180, 'Damn son')
     return flask.Response(status=200)
 
