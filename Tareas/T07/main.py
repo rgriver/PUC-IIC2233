5d1b5059b7c8e2566289085b12dee03f1f0c4a3d
Rodrigo Rivera <rgrivera@uc.cl>
2017-06-25 03:06:55 -0400

    update

--- Tareas/T07/main.py
+++ Tareas/T07/main.py
@@ -145,43 +145,40 @@ class MyApp(flask.Flask):
     def __init__(self):
         super(MyApp, self).__init__(__name__)
         self.bot_controller = BotController()
-        self.configure_routes()
+        self.add_url_rule('/', view_func=self.index)
+        self.add_url_rule('/github', view_func=self.handle_github_event,
+                          methods=['POST'])
+        self.add_url_rule('/telegram', view_func=self.handle_telegram_event,
+                          methods=['POST'])
 
-    def configure_routes(self):
-        @self.route('/github', methods=['POST'])
-        def handle_github_event():
-            try:
-                data = json.loads(flask.request.data)
-                action = data['action']
-                if str(action) == 'opened':
-                    self.bot_controller.notify_of_issue_opening(data['issue'])
-            except Exception as e:
-                message = str(e)
-                self.bot_controller.send_message_to_all_users(message)
-            return flask.Response(status=200)
+    def handle_github_event(self):
+        try:
+            data = json.loads(flask.request.data)
+            action = data['action']
+            if str(action) == 'opened':
+                self.bot_controller.notify_of_issue_opening(data['issue'])
+        except Exception as e:
+            message = str(e)
+            self.bot_controller.send_message_to_all_users(message)
+        return flask.Response(status=200)
 
-        @self.route('/telegram', methods=['POST'])
-        def handle_telegram_event():
-            chat_id = 375779180
-            try:
-                data = json.loads(flask.request.data)
-                message = data['message']['text']
-                chat_id = data['message']['chat']['id']
-            except Exception as e:
-                message = 'INTERNAL SERVER ERROR: ' + str(e)
-            # bot_controller.send_message(chat_id, message)
-            self.bot_controller.process_message(chat_id, message)
-            return flask.Response(status=200)
+    def handle_telegram_event(self):
+        chat_id = 375779180
+        try:
+            data = json.loads(flask.request.data)
+            message = data['message']['text']
+            chat_id = data['message']['chat']['id']
+        except Exception as e:
+            message = 'INTERNAL SERVER ERROR: ' + str(e)
+        # bot_controller.send_message(chat_id, message)
+        self.bot_controller.process_message(chat_id, message)
+        return flask.Response(status=200)
 
-        @self.route('/')
-        def index():
-            """
-            text = "Lista de ID's:\n"
-            for chat_id in self.bot_controller.chat_ids:
-                text += str(chat_id) + '\n'
-            return text
-            """
-            return 'ok'
+    def index(self):
+        text = "Lista de ID's:\n"
+        for chat_id in self.bot_controller.chat_ids:
+            text += str(chat_id) + '\n'
+        return text
 
 """
 bot_controller = BotController()
