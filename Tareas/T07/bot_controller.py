399c8b873dade85c3e48c91ccf8aa6f052ed6987
Rodrigo Rivera <rgrivera@uc.cl>
2017-06-25 03:41:27 -0400

    update

---
+++ Tareas/T07/bot_controller.py
@@ -0,0 +1,52 @@
+import requests
+from repository_controller import RepositoryController
+from command_interpreter import CommandInterpreter
+
+
+class BotController:
+    def __init__(self):
+        self.token = '415058552:AAH_h5aHopemW9hqMhEZpq1Ajg5LLRunhAM'
+        self.telegram_url = 'https://api.telegram.org/bot' + self.token + '/'
+        self.chat_ids = []
+        self.repo_controller = RepositoryController()
+        self.command_interpreter = CommandInterpreter()
+
+    def send_message(self, chat_id, text):
+        params = {'chat_id': chat_id, 'text': text}
+        method = 'sendMessage'
+        requests.post(self.telegram_url + method, params)
+
+    def process_message(self, chat_id, input_message):
+        if chat_id not in self.chat_ids:
+            self.chat_ids.append(chat_id)
+        groups = self.command_interpreter.process_text(input_message)
+        if groups[0] == '/get':
+            message = self.repo_controller.get_issue(*groups[1:])
+        elif groups[0] == '/post':
+            message = self.repo_controller.create_comment(*groups[1:])
+        elif groups[0] == '/label':
+            message = self.repo_controller.add_label(*groups[1:])
+        elif groups[0] == '/close':
+            message = self.repo_controller.close_issue(*groups[1:])
+        else:
+            message = groups[1]
+        self.send_message(chat_id, message)
+
+    def comment_on_issue(self, issue_num):
+        pass
+
+    def send_message_to_all_users(self, message):
+        for chat_id in self.chat_ids:
+            self.send_message(chat_id, message)
+
+    def notify_of_issue_opening(self, issue):
+        try:
+            message = "[{}]\n".format(issue['user']['login'])  # author
+            message += "[#{} - {}]\n".format(issue['number'], issue['title'])
+            message += issue['body'] + '\n'
+            message += "[Link: {}]".format(issue['html_url'])
+            for chat_id in self.chat_ids:
+                self.send_message(chat_id, message)
+        except Exception as e:
+            for chat_id in self.chat_ids:
+                self.send_message(chat_id, 'INTERNAL SERVER ERROR: ' + str(e))
