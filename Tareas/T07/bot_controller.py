c74c57a66a108ee5250aed79311cbd4eecf8b72e
Rodrigo Rivera <rgrivera@uc.cl>
2017-06-26 02:45:54 -0400

    update

--- Tareas/T07/bot_controller.py
+++ Tareas/T07/bot_controller.py
@@ -7,7 +7,6 @@ class BotController:
     def __init__(self, repo_controller):
         self.token = '415058552:AAH_h5aHopemW9hqMhEZpq1Ajg5LLRunhAM'
         self.telegram_url = 'https://api.telegram.org/bot' + self.token + '/'
-        self.chat_ids = []
         self.repo_controller = repo_controller
         self.command_interpreter = CommandInterpreter()
 
@@ -17,7 +16,7 @@ class BotController:
         requests.post(self.telegram_url + method, params)
 
     def process_message(self, chat_id, input_message):
-        if chat_id not in self.chat_ids:
+        if chat_id not in self.get_chats():
             self.add_chat(chat_id)
             # self.chat_ids.append(chat_id)
         groups = self.command_interpreter.process_text(input_message)
@@ -37,7 +36,7 @@ class BotController:
         pass
 
     def send_message_to_all_users(self, message):
-        for chat_id in self.chat_ids:
+        for chat_id in self.get_chats():
             self.send_message(chat_id, message)
 
     def notify_of_issue_opening(self, issue):
@@ -50,7 +49,7 @@ class BotController:
             for chat_id in chat_ids:
                 self.send_message(chat_id, message)
         except Exception as e:
-            for chat_id in self.chat_ids:
+            for chat_id in self.get_chats():
                 self.send_message(chat_id, 'INTERNAL SERVER ERROR: ' + str(e))
 
     @staticmethod
